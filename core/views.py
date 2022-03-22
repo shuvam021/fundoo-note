1e19a2f1623412468bdd2bca0995f634063ccdb7
shuvam021 <career.shuvam@gmail.com>
2022-03-22 08:04:38 +0530

    [Shuvam] Refactor html forms for login/register view

--- core/views.py
+++ core/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView
-from django.urls import reverse
 from core.forms import LoginForm, RegisterForm
 
 logger = logging.getLogger(__name__)
@@ -15,17 +14,19 @@ def fbv_login_view(request):
     form = LoginForm(auto_id=False)
     if request.method == 'POST':
         try:
-            email=request.POST['email']
-            password=request.POST['password']
-            user = authenticate(request=request, email=email, password=password)
+            user = authenticate(
+                request=request,
+                email=request.POST['email'],
+                password=request.POST['password']
+            )
             if user is not None:
                 login(request, user)
                 return redirect('profile')
             messages.error(request, 'Login Failed')
-            return redirect('custom-login', context={'form': form, 'message': 'Login Failed'})
+            return redirect('custom-login')
         except Exception as e:
             logger.exception(e)
-    return render(request, 'login.html', context={'form': form})
+    return render(request, 'forms.html', context={'form': form, 'title': 'Login'})
 
 
 def fbv_logout_view(request):
@@ -35,5 +36,10 @@ def fbv_logout_view(request):
 
 class UserResister(CreateView):
     form_class = RegisterForm
-    template_name = 'register.html'
+    template_name = 'forms.html'
     success_url = '/login/'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['title'] = 'Register'
+        return data
